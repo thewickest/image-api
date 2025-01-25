054d4ab1cfb70e32b9fcfbc6e0a7234379a25ea1
thewickest <graubuntu@gmail.com>
2025-01-25 12:54:31 +0100

    Resize function first approach

--- api/src/tasks/tasks.service.ts
+++ api/src/tasks/tasks.service.ts
@@ -2,15 +2,53 @@ import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { TaskDto } from './dto/task.dto';
+const sharp =  require('sharp');
+const fs = require('node:fs')
+const path = require('node:path')
 
 @Injectable()
 export class TasksService {
   create(createTaskDto: CreateTaskDto): TaskDto {
-    return {
-      taskId: 'asdfas',
+    const inputPath = createTaskDto?.path
+    const outputFolder = './output'
+    const sizes = [800, 1024]
+
+    const extImage = path.extname(inputPath)
+    const imageName = path.basename(inputPath, extImage)
+
+    // this should be done by mongdb
+    const task = {
+      taskId: (Math.floor(Math.random() * 1000) + 1).toString(),
       status: 'pending',
-      price: Math.random()
+      originalPath: createTaskDto.path,
+      price: Math.floor(Math.random() * 50) + 1
     }
+
+    fs.appendFileSync('./data.json', JSON.stringify(task), err => {
+      if(err) {
+        console.error(err)
+      } else {
+        // succesfully
+      }
+    })
+
+    for (const size of sizes) {
+      const outputPath = `${outputFolder}/${imageName}/${size}`
+
+      if (!fs.existsSync(outputPath)) {
+        fs.mkdirSync(outputPath, { recursive: true });
+      }
+
+      sharp(inputPath)
+        .resize({width: size})
+        // change this to md5
+        .toFile(`${outputPath}/${imageName}-${size}${extImage}`)
+        .then(data => {
+          fs.appendFileSync('./data.json', 'completed')
+        })
+    }
+
+    return task;
   }
 
   findOne(id: number) {
